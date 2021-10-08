35bb490540033a9868bd98977122835408ee8fba
yeffry <yeffry45@gmail.com>
2021-10-07 21:41:24 -0400

    add basic factory Ship types for testing

---
+++ index.js
@@ -0,0 +1,45 @@
+const shipLength = {
+  carrier: 5,
+  battleship: 4,
+  cruiser: 3,
+  submarine: 3,
+  destroyer: 2,
+}
+
+const shipName = [
+  'carrier',
+  'battleship',
+  'cruiser',
+  'submarine',
+  'destroyer',
+];
+
+const Ship = (shipType) => {
+  const type = shipType
+  const length = shipLength[shipType]
+
+  const hit = (num) => {
+    //  function that takes a number and then marks that position as ‘hit’.
+  }
+
+  const isSunk = () => {
+    // should be a function that calculates it based on their length and whether all of their positions are ‘hit’.
+  }
+
+  return {
+    type,
+    length,
+    hit,
+    isSunk,
+  }
+}
+
+
+const shipFleet = {}
+
+shipName.forEach((shipType) => {
+  shipFleet[shipType] = Ship(shipType)
+})
+
+console.log(shipFleet)
+
