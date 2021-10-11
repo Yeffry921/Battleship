89a9392bb448fa1b57773f56934f6a81f940e588
yeffry <yeffry45@gmail.com>
2021-10-11 14:16:31 -0400

    add logic for the hit function

--- index.js
+++ index.js
@@ -18,33 +18,22 @@ const Ship = (shipType) => {
   const type = shipType
   const length = shipLength[shipType]
 
+
   const hit = (num) => {
-    //  function that takes a number and then marks that position as ‘hit’.
+    shipHits[num] = 'hit'
   }
 
   const isSunk = () => {
-    // should be a function that calculates it based on their length and whether all of their positions are ‘hit’.
+   
   }
 
   return {
     type,
     length,
+    shipHits,
     hit,
     isSunk,
   }
 }
 
-const createShipFleet = () => {
-  const shipFleet = {}
-
-  shipName.forEach((shipType) => {
-  shipFleet[shipType] = Ship(shipType)
-  })
-
-  return shipFleet
-
-}
-
-
-
-
+module.exports = Ship
