57e769a0e5dbdffc1b0b39fed50a65ac1bb0e28b
yeffry <yeffry45@gmail.com>
2021-10-28 18:30:18 -0400

    update config settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,25 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/index.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
   },
-  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "battleship",
-    }),
+      title: 'Battleship',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    })
   ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']}, 
+      { test: /\.css$/, use: ['style-loader', 'css-loader']}
+    ],
+  },
+  mode: 'development'
 }
